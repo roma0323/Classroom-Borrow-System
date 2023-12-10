0595fb1c1342b4e84f0f2b215f1877ff14a493f4
danwan1027 <danwan1027@gmail.com>
2023-12-10 16:42:46 +0800

    feat: equipment CRUD & 連接資料庫的文件配置 & 新增 db_document
    調整項目 -- equipment CRUD：
    1. 新增 Equipment 資料夾
    2. 新增 Equipment
    3. 新增 EquipmentController
    4. 新增 EquipmentService
    5. 新增 EquipmentRepository
    6. 新增 editEquipment.html
    7. 新增 findAllEquipment.html
    
    調整項目 -- 連接資料庫的文件配置：
    1. 新增 application.properties.example 來連結 mysql 資料庫
    2. 更改pom.xml 來連結 mysql 資料庫
    
    調整項目 -- 新增 db_document：
    1. 新增 Classroom_Equipment_Booking_System.sql
    2. 新增 Classroom_Equipment_Booking_System.sql

---
+++ src/main/java/com/example/servingwebcontent/Daniel/Equipment/EquipmentService.java
@@ -0,0 +1,33 @@
+package com.example.servingwebcontent.Daniel.Equipment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EquipmentService {
+
+    private final EquipmentRepository equipmentRepository;
+
+    @Autowired
+    public EquipmentService(EquipmentRepository equipmentRepository){
+        this.equipmentRepository = equipmentRepository;
+    }
+
+
+    public List<Equipment> findAll() {return equipmentRepository.findAll();}
+
+    public void save(Equipment newEquipment){
+        equipmentRepository.save(newEquipment);
+    }
+
+    public void deleteById(Long equipmentId){
+        equipmentRepository.deleteById(equipmentId);
+    }
+
+    public Optional<Equipment> findById(Long equipmentId){
+        return equipmentRepository.findById(equipmentId);
+    }
+}
