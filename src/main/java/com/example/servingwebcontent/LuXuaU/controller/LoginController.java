76833c6f664869ca8141afea8d43a7ecebd678e3
tofudoctor <87269360+tofudoctor@users.noreply.github.com>
2023-12-24 03:10:47 +0800

    feat: login auth complete

--- src/main/java/com/example/servingwebcontent/LuXuaU/controller/LoginController.java
+++ src/main/java/com/example/servingwebcontent/LuXuaU/controller/LoginController.java
@@ -1,12 +1,16 @@
 package com.example.servingwebcontent.LuXuaU.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    String login() {
+    String login() {;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode("admin");
+        System.out.println(encodedPassword);
         return "LuXuaU/login";
     }
 }
