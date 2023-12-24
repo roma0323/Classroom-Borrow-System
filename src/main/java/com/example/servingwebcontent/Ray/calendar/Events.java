c4ad4b664e86b9d5d99611635a313e77a353150f
romaspare <r911223oger@yahoo.com>
2023-12-25 05:10:02 +0800

    feat:get backend data in calendar

---
+++ src/main/java/com/example/servingwebcontent/Ray/calendar/Events.java
@@ -0,0 +1,58 @@
+package com.example.servingwebcontent.Ray.calendar;
+
+import com.google.gson.Gson;
+
+public class Events {
+    private Long id;
+    private String title;
+    private String start;
+    private String end;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String toString() {
+        return "Events{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", start='" + start + '\'' +
+                ", end='" + end + '\'' +
+                '}';
+    }
+
+    // Method to convert to JSON using Gson library
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+}
--- src/main/java/com/example/servingwebcontent/Ray/calendar/calendarController.java
+++ src/main/java/com/example/servingwebcontent/Ray/calendar/calendarController.java
@@ -1,18 +1,24 @@
 package com.example.servingwebcontent.Ray.calendar;
 
 import com.example.servingwebcontent.Ray.Booking.Booking;
-import com.example.servingwebcontent.Ray.Person;
+import com.example.servingwebcontent.Ray.Booking.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
 
 @Controller
 public class calendarController {
 
+    private final BookingService bookingService;
+    @Autowired
+    public calendarController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
     @GetMapping("/weekly_calendar")
     public String showWeeklyCalendar() {
         return "Ray/calendar/calendar"; // Return the name of the HTML file without the extension
@@ -21,7 +27,24 @@ public class calendarController {
     @GetMapping("/test")
     public ModelAndView bookingDetail() {
         ModelAndView modelAndView = new ModelAndView("Ray/calendar/test_calendar");
-        modelAndView.addObject("pass", "通過");
+        Iterable<Booking> bookingList = bookingService.findAll();
+        modelAndView.addObject("bookingList", bookingList);
+        List<Events> EventsList = new ArrayList<>();
+        for (Booking booking : bookingList) {
+            Events event = new Events();
+            System.out.println(booking.getName());
+
+            event.setTitle(booking.getName());
+            event.setId(booking.getId_booking());
+            event.setStart(booking.getStart_time());
+            event.setEnd(booking.getEnd_time());
+            EventsList.add(event);
+            System.out.println("end"+event.getEnd());
+
+        }
+        modelAndView.addObject("testJson",EventsList);
+
+
         return modelAndView;
     }
 }
